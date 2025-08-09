90e25186553360dff1b5d6640e8556f023700481
Nywton Barros <nywton.barros@gmail.com>
2025-08-09 03:38:19 -0300

    feat: Enables solid cache on dev environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,11 +20,12 @@ Rails.application.configure do
   if Rails.root.join("tmp/caching-dev.txt").exist?
     config.public_file_server.headers = { "cache-control" => "public, max-age=#{2.days.to_i}" }
   else
-    config.action_controller.perform_caching = false
+    config.action_controller.perform_caching = true
   end
 
   # Change to :null_store to avoid any caching.
-  config.cache_store = :memory_store
+  # config.cache_store = :memory_store, { size: 64.megabytes }
+  config.cache_store = :solid_cache_store
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
