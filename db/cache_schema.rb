60e894512b006c67c1607414d40cc6536cfaaa6e
Nywton Barros <nywton.barros@gmail.com>
2025-08-09 03:32:26 -0300

    feat: Enables solid cache to development environment

--- db/cache_schema.rb
+++ db/cache_schema.rb
@@ -1,6 +1,16 @@
-# frozen_string_literal: true
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 1) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_08_160920) do
   create_table "solid_cache_entries", force: :cascade do |t|
     t.binary "key", limit: 1024, null: false
     t.binary "value", limit: 536870912, null: false
@@ -11,4 +21,26 @@ ActiveRecord::Schema[7.2].define(version: 1) do
     t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
     t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
   end
+
+  create_table "transactions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "from_currency"
+    t.string "to_currency"
+    t.decimal "from_value", precision: 18, scale: 2, null: false
+    t.decimal "to_value", precision: 18, scale: 2, null: false
+    t.decimal "rate", precision: 18, scale: 4, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_transactions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
+  add_foreign_key "transactions", "users"
 end
