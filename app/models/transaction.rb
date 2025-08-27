aad413abeb53859dc9cc2ef204b52d0494e83751
Nywton Barros <nywton.barros@gmail.com>
2025-08-27 00:33:13 -0300

    feat: Updates ruby

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,23 +2,23 @@ class Transaction < ApplicationRecord
   belongs_to :user
 
   validates :from_currency, :to_currency,
-            presence: true,
-            inclusion: {
-              in:      Currencies::CODES,
-              message: "%{value} is not a supported currency code"
-            }
+    presence: true,
+    inclusion: {
+      in:      Currencies::CODES,
+      message: "%{value} is not a supported currency code"
+    }
 
   validates :from_value, :to_value,
-            numericality: {
-              greater_than_or_equal_to: -9_999_999_999_999_999.99,
-              less_than_or_equal_to:    9_999_999_999_999_999.99
-            }
+    numericality: {
+      greater_than_or_equal_to: -9_999_999_999_999_999.99,
+      less_than_or_equal_to:    9_999_999_999_999_999.99
+    }
 
   validates :rate,
-            numericality: {
-              greater_than_or_equal_to: 0.0001,
-              less_than_or_equal_to:    99_999_999_999_999.9999
-            }
+    numericality: {
+      greater_than_or_equal_to: 0.0001,
+      less_than_or_equal_to:    99_999_999_999_999.9999
+    }
 
   alias_attribute :timestamp, :created_at
 
